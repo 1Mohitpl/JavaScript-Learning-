489572a9b832a96543f0d1581cecc21a6155ed5f
Mohit Kumar Paul <142975243+1Mohitpl@users.noreply.github.com>
2023-12-08 15:11:07 +0000

    confirm commit

---
+++ learn.js
@@ -0,0 +1,16 @@
+console.log('hello world')
+console.log('by by raiganj')
+
+
+let  accountemial = "gurbnd@gmail.com"
+var accoutPassword  ="7555586"
+accountcity = "wb"
+/* prefer not to use var becasue of issues in block scope and functional scope */
+
+
+accountemial = "mkfnf@jaddf"
+accoutPassword = "855660"
+accountcity = "assam"
+
+console.log(accountemial);
+console.table([accountemial,accoutPassword,accountcity])
