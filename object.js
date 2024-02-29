a709463fd36e18cc40d51b5549e6c9756c4e8481
Mohit Kumar Paul <142975243+1Mohitpl@users.noreply.github.com>
2024-02-29 17:05:07 +0530

    git commit object create

---
+++ object.js
@@ -0,0 +1,42 @@
+// alert("Hello")[1, 2].forEach(alert);
+
+
+// let reactangle = {           // object create 
+
+//     lenght: 1 ,
+//     breadth: "mohit",
+
+//     draw :function(){
+//     console.log('drawing reactangle');
+//     }
+    
+// };
+
+//   console.log (reactangle.lenght);
+//   console.log (reactangle[draw]);
+
+
+function creatReactangle (len, bre) {
+
+    return reactAngle  = {          
+
+        lenght: len,
+        breadth: bre,
+    
+        draw (){
+        console.log('drawing reactangle');
+        }
+        
+    };
+
+
+  }
+    
+   let reactAngleobj1 = creatReactangle(5,4)
+   console.log(reactAngleobj1);
+
+
+
+  //  let reactangleobj1 = creatReactangle();
+  //  console.log(reactangleobj1.draw);
+
