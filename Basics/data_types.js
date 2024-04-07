965b1b5c623a000d5d1377eafb6e193bab801e65
Mohit kumar paul <mohit1paul@gmail.com>
2024-04-07 19:50:23 +0530

    basics

---
+++ Basics/data_types.js
@@ -0,0 +1,63 @@
+"use strict"; // treat all js code as newr version //
+
+//  alert (3+3) // we are using node js not browser , 
+
+
+// console.log(3+3)
+//alert(3+3)
+// let name = "mohit"
+// let age = 22;
+// let isadmin = false
+// console.log(name, age, isadmin)
+
+// let score = "332ddf";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueofnumber = Number (score)       // conservation of a value of a number
+                                        // "33"=>33;
+                                       // "33addc" => Nan
+                                       // True/false => 0/1;
+//                                        //  
+// console.log(typeof valueofnumber);  
+// console.log(valueofnumber);
+
+
+let  isboolean = "mohit"
+
+let booleanisloggedIn = Boolean(isboolean)  // booleamn conversion of a number
+console.log(booleanisloggedIn)
+
+
+let putnumber = 44;
+let stringnumber = String(putnumber)
+console.log(stringnumber)
+console.log(typeof (stringnumber))   // conversion of number into the string data types
+
+
+
+// lets learn operations 
+
+let fisrtname = "Mohit"
+let lastname = " Paul"
+
+let fullname = fisrtname + lastname
+console.log(fullname)
+
+console.log(1+2)
+console.log("1" +2)
+console.log(1+"2")
+
+console.log("1" + "2")
+console.log(1+2+"2")
+console.log("1" + "2" +1)
+
+console.log(1+2+2)
+
+console.log ( 4+46)
+
+
+let gamecounter  = 100;
+gamecounter ++;
+console.log(gamecounter);
---
+++ Basics/object.js
@@ -0,0 +1,164 @@
+// alert("Hello")[1, 2].forEach(alert);
+
+
+let reactangle = {           // object create 
+
+    lenght: 1 ,
+    breadth: "mohit",
+
+    draw :function(){
+    console.log('drawing reactangle');
+    }
+    
+};
+
+//   console.log (reactangle.lenght);
+//   console.log (reactangle[draw]);
+
+// factory function 
+// function creatReactangle (len, bre) {
+
+//     return reactAngle  = {          
+
+//         lenght: len,
+//         breadth: bre,
+    
+//         draw (){
+//         console.log('drawing reactangle');
+//         }
+        
+//     };
+
+
+//   }
+     
+    //  reactAngle.draw();
+//    let reactAngleobj1 = creatReactangle(5,4)
+//    console.log(reactAngleobj1);
+
+
+
+  //  let reactangleobj1 = creatReactangle();
+  //  console.log(reactangleobj1.draw);
+
+
+
+  // constructor function => pascal notation => first letter of every word is capital
+
+  function Mycar (col, collec, len) {
+    
+    this.color = col;
+    this.collection = collec;
+    this.lenght = len;
+    this.size = 13;
+    this.draw = function () {
+
+        console.log(this.size);
+    }
+
+  }
+
+  // object creatiion using constructor funtion
+
+let mycarObject  = new Mycar ( "green" , 4, 5 );
+console.log(mycarObject);
+
+mycarObject.speed = 40;        // add a new property 
+console.log(mycarObject);
+
+delete mycarObject.size;       // delete an property from an object
+console.log(mycarObject);
+
+console.log(mycarObject.constructor);
+console.log(Mycar.constructor);
+
+
+let Mycar1 = new Function('len', 'collec', 'col' , 
+` 
+this.color = col;
+this.collection = collec;
+this.lenght = len;
+this.size = 13;
+this.draw = function () {
+
+    console.log(this.size);
+}`);
+
+ // object creation using mycar1 
+ let rec = new  Mycar1(2);
+ console.log(rec);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let's create another function 
+
+//  function  Myhouse  ( col, price, add) {
+
+//           this.color = col;
+//           this.priceofHouse = price;
+//           this.address = add;
+//  }
+
+
+//  let MyhouseObject = new Myhouse ("Black" , "1cr" , "India")
+
+//  console.log(MyhouseObject);
+
+
+//  MyhouseObject.size = "2bhk";
+//  console.log(MyhouseObject);     // add new property 
+
+
+
+//  delete MyhouseObject.color;
+// console.log(MyhouseObject);    // delete an property
+
+
+
+
+// object cloning using iteration methode
+
+let Car = {
+   
+  color : "Red",
+
+  price : "2cr",
+
+  Model  : "suv"
+  
+
+};
+
+
+
+// for (let key in Car ){
+
+//     dest [key] = Car[key];                // object cloning using iteration methode
+// }
+
+// console.log (dest)
+
+let dest = Object.assign({}, Car);                //object cloning using Assign methode
+console.log(dest) 
+
+
+let area = {...Car}                                      // object cloing using spread methode
+console.log(area)
+
+
+
+let myName = `mohit
+kumar 
+paul `                                    // Template literal ` ` to get the string as order as get the value from console
+
+console.log(myName)
