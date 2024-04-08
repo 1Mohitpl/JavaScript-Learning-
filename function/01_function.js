5978aa295bd490d5efbfd219d8f2bef568d3a006
Mohit kumar paul <mohit1paul@gmail.com>
2024-04-09 01:10:08 +0530

    function start

---
+++ function/01_function.js
@@ -0,0 +1,22 @@
+// function addTwoNumbers (number1, number2) {
+
+//     console.log(number1 + number2)
+// }
+
+
+function addTwoNumbers (number1, number2) {
+
+    let result = (number1 + number2)
+    return result
+}
+const final = addTwoNumbers(7,4)
+
+console.log(final)
+
+
+function loginUserMessage(username){
+
+      return `${username} just logged in`
+}
+
+console.log(loginUserMessage("mohit"))
