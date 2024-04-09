2294ce7ad0c7280cf30bbad09e4ab9b211cc75aa
Mohit kumar paul <mohit1paul@gmail.com>
2024-04-09 10:59:31 +0530

    function complete

--- function/01_function.js
+++ function/01_function.js
@@ -20,3 +20,51 @@ function loginUserMessage(username){
 }
 
 console.log(loginUserMessage("mohit"))
+
+
+function  calculatePrice (val1, val2, ...num1) {     // ... rest opertaor to use pass the multiple value as parameter 
+
+     return num1
+}
+
+console.log(calculatePrice(200,400,460))
+
+
+
+// create an object that pass into the function 
+
+
+const user = {
+     
+    username :"mohit",
+    price : 200
+}
+
+
+
+function handleObject(anyobject ){
+
+      console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+
+
+}
+
+// handleObject(user)
+
+handleObject ({              // passing object in function 
+
+     username : "rahul",       
+     price: 399
+})
+
+
+const myNewarray = [300, 605,893,900]
+
+function returnSecondvalue (getarray){
+
+    return getarray[2]
+}
+
+console.log(returnSecondvalue(myNewarray))
+
+console.log(returnSecondvalue([300,100,900]))    // pass a new array 
