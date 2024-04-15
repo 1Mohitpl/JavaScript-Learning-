b6ace7909a207b24fe61818558a1406e54601bee
Mohit kumar paul <mohit1paul@gmail.com>
2024-04-15 18:47:10 +0530

    arrow function

--- function/arrow.js
+++ function/arrow.js
@@ -2,5 +2,42 @@ const user = {
 
     username : "mohit",
     price : 999,
-    
+
+    welcomemessage : function(){
+
+        console.log(`${this.username},  Welcome to the website`)
+    }
 }
+
+user.welcomemessage()
+user.username ="rahul"
+user.welcomemessage()
+
+
+// const life = function(){
+
+//      let username = "mohit paul"
+//      console.log(this.username);
+// }
+
+const life = () =>{                      // arrow function syntax
+
+    let username = "mohit paul"
+    console.log(this.username);
+}
+
+console.log(life())
+
+
+const addTwo = (num1, numb2) =>{
+        
+    return num1 +numb2                            // arrow function 
+
+}
+
+console.log(addTwo(3,9));
+
+
+const addTwonumber = (num1, numb2) => ( num1 +numb2);   // implicit return 
+
+console.log(addTwonumber(4,6))
