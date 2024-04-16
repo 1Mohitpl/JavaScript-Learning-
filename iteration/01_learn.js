0b2083226b2fab243dd4f07bec98c81b7e77184a
Mohit kumar paul <mohit1paul@gmail.com>
2024-04-16 14:15:18 +0530

    iteration

---
+++ iteration/01_learn.js
@@ -0,0 +1,21 @@
+// for loop 
+
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+    console.log(element);
+    
+}
+
+// break and continew 
+
+for (let index = 0; index <=10; index++) {
+    if (index == 4) {
+
+        console.log(`it is found`)
+        continue;
+        
+    }
+    console.log(`value of index is : ${index}`)
+    
+}
+
