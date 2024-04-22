9701655810c653796ec73828108ed521e3f75e83
Mohit kumar paul <mohit1paul@gmail.com>
2024-04-23 01:24:00 +0530

    all codes

--- Promise/02_learn.js
+++ Promise/02_learn.js
@@ -1,65 +1,65 @@
-// const cart  = ["shirts", "shoes","toy"]
+const cart  = ["shirts", "shoes","toy"]
 
-// // createOrder(cart, function (orderId){
-// //     procedTopayment(orderId);
-// // });   // return order detils
+// createOrder(cart, function (orderId){
+//     procedTopayment(orderId);
+// });   // return order detils
 
-// createOrder(cart)
+createOrder(cart)
 
-//   // promise channing                 // console.log(promise);
-// .then(function (orderId){
-//     console.log(orderId);
-//     return orderId;
-// })
-// .catch (function(error) {
-//     console.log(error.message);
-// })
-// .then(function(orderId) {
-//     return procedTopayment(orderId)
-// })
-//  .then(function (paymentInfo) {
-//     console.log(paymentInfo)
-//  })
+  // promise channing                 // console.log(promise);
+.then(function (orderId){
+    console.log(orderId);
+    return orderId;
+})
+.catch (function(error) {
+    console.log(error.message);
+})
+.then(function(orderId) {
+    return procedTopayment(orderId)
+})
+ .then(function (paymentInfo) {
+    console.log(paymentInfo)
+ })
 
-// .catch (function(error) {
-//     console.log(error.message);
-// })   
+.catch (function(error) {
+    console.log(error.message);
+})   
 
-//proudcer
+proudcer
 
-// function createOrder(cart) {
+function createOrder(cart) {
 
-//     const prom = new Promise(function (resolve, reject) {
-//           //createorderid
-//           //validcart
-//           //orderid
-//         if(!validcart(cart)){
+    const prom = new Promise(function (resolve, reject) {
+          //createorderid
+          //validcart
+          //orderid
+        if(!validcart(cart)){
           
-//             const error = new Error("cart is not valid");
-//             reject(error);
-//         }
+            const error = new Error("cart is not valid");
+            reject(error);
+        }
 
-//         //logic for orderid
+        //logic for orderid
 
-//         const orderid  = "76833";
-//         if(orderid){
-//             setTimeout(function (){
-//                 resolve(orderid);
-//             }, 4000);
-//         }
-//     });
+        const orderid  = "76833";
+        if(orderid){
+            setTimeout(function (){
+                resolve(orderid);
+            }, 4000);
+        }
+    });
 
-//     return prom;
-// }
+    return prom;
+}
 
-// function procedTopayment(orderId){
-//     return new Promise (function (resolve, reject) {
-//         resolve("payment is done")
-//     });
-// }
-// function validcart(cart){
-//     return false;
-// }
+function procedTopayment(orderId){
+    return new Promise (function (resolve, reject) {
+        resolve("payment is done")
+    });
+}
+function validcart(cart){
+    return false;
+}
 
  // +++++++++++++++++++++++++++++++++++++++++++++++++++++
 // Learning ALL promise API
@@ -94,3 +94,12 @@ Promise.any([P1, P2, P3])
     console.error(err)
     console.log(err.errors);
   })
+
+
+
+  const P4 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("p1 sucess")
+    }, 4000)
+})
+
