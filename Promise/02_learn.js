181c883bbe7e077c09da34c6b8743f8eae1179c5
Mohit kumar paul <mohit1paul@gmail.com>
2024-04-22 20:16:39 +0530

    catch methode to handle the error

--- Promise/02_learn.js
+++ Promise/02_learn.js
@@ -1,14 +1,50 @@
 const cart  = ["shirts", "shoes","toy"]
 
-createOrder(cart, function (orderId){
-    procedTopayment(orderId);
-});   // return order detils
-
+// createOrder(cart, function (orderId){
+//     procedTopayment(orderId);
+// });   // return order detils
 
 const promise = createOrder(cart);
 
+// console.log(promise);
+
+
 promise.then(function (orderId){
-    procedTopayment(orderId)
+    console.log(orderId);
+    // procedTopayment(orderId)
 })
 
+.catch (function(error) {
+    console.log(error.message);
+})
 
+//proudcer
+
+function createOrder(cart) {
+
+    const prom = new Promise(function (resolve, reject) {
+          //createorderid
+          //validcart
+          //orderid
+        if(!validcart(cart)){
+          
+            const error = new Error("cart is not valid");
+            reject(error);
+        }
+
+        //logic for orderid
+
+        const orderid  = "76833";
+        if(orderid){
+            setTimeout(function (){
+                resolve(orderid);
+            }, 4000);
+        }
+    });
+
+    return prom;
+}
+
+function validcart(cart){
+    return false;
+}
