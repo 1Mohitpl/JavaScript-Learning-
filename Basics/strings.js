c8afd268e098eb952f830438c25c3b5ed67f89cf
Mohit kumar paul <mohit1paul@gmail.com>
2024-07-30 19:06:14 +0530

    practice string

--- Basics/strings.js
+++ Basics/strings.js
@@ -2,10 +2,18 @@ const myName = "Mohit";
 const age = 23;
 
 
-// console.log(`Hello my name is ${myName} and my age is ${age}`);
+console.log(`Hello my name is ${myName} and my age is ${age}`);  // string interpoletion, modern string syntax
 
 
 
 const newstringone = "    mohit  "
 console.log(newstringone)
-console.log(newstringone.trim())
+console.log(newstringone.trim())    // Removing the space within the string
+
+const gamename = new String('mohitlc');
+console.log(gamename[0]);
+
+console.log(gamename.toUpperCase());
+console.log(gamename);
+console.log(gamename.charAt(3));     // check the postion of char by their positon 
+console.log(gamename.indexOf('t'));  // check the index of char
