60309aa000b6e28a14f28b40307e3e7d30b8620b
Mohit kumar paul <mohit1paul@gmail.com>
2024-08-28 23:31:08 +0530

    filter function

--- MAP/map.js
+++ MAP/map.js
@@ -1,16 +1,62 @@
-const arr = [5, 2,3,1, 6]
+// const arr = [5, 2,3,1, 6]
 
-// double  - [10, 4, 6, 2, 12]
+// // double  - [10, 4, 6, 2, 12]
 
-function double(x){
-    return x*2;
-}
+// function double(x){
+//     return x*2;
+// }
 
-function binary(a){
-    return a.toString(2);
-}
+// function binary(a){
+//     return a.toString(2);
+// }
 
-const output = arr.map(double);
-const output2 = arr.map(binary);
+// const output = arr.map(double);
+// const output2 = arr.map(binary);
+// console.log(output);
+// console.log(output2);
+
+
+// example of another map function
+
+const users = [
+    {fisrtName: "ram", lastName: "saha", Age: 23},
+    {fisrtName: "mohit", lastName: "paul", Age: 23},
+    {fisrtName: "neha", lastName: "saha", Age: 27}
+];
+
+// findout the list of full name
+
+const output = users.map((X) => X.fisrtName + " " + X.lastName);
 console.log(output);
-console.log(output2);
+
+
+// findout the number of pepole in different level of age
+
+const outputAge = users.reduce((acc, curr) => {
+    if(acc[curr.Age]){
+        acc[curr.Age] = ++ acc[curr.Age];
+
+    } else{
+        acc[curr.Age] = 1;
+    }
+     return acc;
+}, {})
+
+console.log(outputAge)
+
+// findout the fisrtname whose age < 26 using filter function
+
+const result = users.filter((X) => X.Age<26).map((X) => X.fisrtName);
+console.log(result);
+
+// perform this into the reduce function
+
+const result2 = users.reduce((acc, curr) => {
+    if(curr.Age<26){
+        acc.push(curr.fisrtName);
+
+    }
+    return acc;
+},[])
+
+console.log(result2);
---
+++ Reduce/reduce.js
@@ -0,0 +1,48 @@
+const arr = [5, 1, 3, 2, 6];
+
+// sum of max;
+
+function findSum(arr){         // this is simple function to find out sum of variable
+
+    let sum = 0;
+    for(let i = 0; i<arr.length; i++){   
+      sum += arr[i];
+    }
+    return sum;
+}
+console.log(findSum(arr));
+
+
+// now reduce the above code with reduce function
+
+const output = arr.reduce(function(acc, curr){
+     acc = acc + curr;
+     return acc;
+}, 0)
+
+console.log(output);
+
+// find out max 
+
+function findMax (arr){
+    let max= 0;
+    for(let i= 0; i<arr.length; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
+
+console.log(findMax(arr));
+
+// write the same code using reduce fuction
+
+const result = arr.reduce((acc, curr) =>{
+    if(curr >acc){
+        acc = curr;
+    }
+    return acc;
+}, 0)
+console.log(result);
