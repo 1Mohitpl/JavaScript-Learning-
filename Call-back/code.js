abb93a3f4fd12c6cce95a8ea48d0388de0760766
Mohit kumar paul <mohit1paul@gmail.com>
2024-08-29 22:25:51 +0530

    call back function

---
+++ Call-back/code.js
@@ -0,0 +1,65 @@
+// call back function
+
+// function x (y) {
+
+// }
+
+// x(function y(){    // call the function x and passing another function as an argument known as Call-back function
+     
+// })
+
+
+// setTimeout(() => {
+//     console.log("timer");
+    
+// }, 5000);
+
+// function x (y) {
+//    console.log("x");
+//    y();
+// }
+
+// x(function y(){    
+//      console.log("y");
+     
+// });
+
+function attach(){
+    let count = 0;
+    document.getElementById("click")
+    .addEventListener("click", () =>{
+    console.log("hey this is mohit", ++ count);
+});
+
+}
+
+attach();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("Namestay");
+
+// setTimeout(function () {
+//     console.log("javascript");
+// }, 5000)      // now it is 
+
+
+// console.log("learning");
+
