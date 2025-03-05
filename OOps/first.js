60e1dc24e75afce90215e512d6bd527cf34968ca
Mohit kumar paul <mohit1paul@gmail.com>
2025-03-05 23:59:37 +0530

    private variable

--- OOps/first.js
+++ OOps/first.js
@@ -1,25 +1,29 @@
 class Product{
     // no-need of use let,var,const
-    name;
-    price;
+    
+    #name;
+    age;
     discription;
 
-    display() {
-        // no-need to write function key word"
-    }
+  
 
-    constructor (name, price, desc){
-          this.name = name;
-          this.price = price;
+    constructor (name, age, desc){
+          this.#name = name;
+          this.age = age;
           this.discription = desc;
           console.log("constructor is called");
           
     }
-
+    display() {
+        // no-need to write function key word"
+     console.log(this.#name, this.age, this.discription);
+     return;
+    }
 
 }
 
-const p = new Product("mohit", 100, "a iphone"); // default constructor
+const p = new Product("mohit", 24, "Software enginner"); // default constructor
 
-console.log(p);
+
+p.display();
 
