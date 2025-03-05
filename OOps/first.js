9e28c44138348f8836aa0e084e77435a51ddf3a3
Mohit kumar paul <mohit1paul@gmail.com>
2025-03-05 21:47:18 +0530

    functionconstructor

--- OOps/first.js
+++ OOps/first.js
@@ -2,16 +2,24 @@ class Product{
     // no-need of use let,var,const
     name;
     price;
-    discount;
     discription;
 
     display() {
         // no-need to write function key word"
-
-
     }
 
-    buy(){
-        
+    constructor (name, price, desc){
+          this.name = name;
+          this.price = price;
+          this.discription = desc;
+          console.log("constructor is called");
+          
     }
+
+
 }
+
+const p = new Product("mohit", 100, "a iphone"); // default constructor
+
+console.log(p);
+
---
+++ OOps/functionConstructor.js
@@ -0,0 +1,9 @@
+// function-constructor
+function Product (name, price, desc) {
+    this.name = name;
+    this.price = price;
+    this.desc = desc;
+}
+
+const properties =  new Product("kitkat", 10, "a good chockalte");
+console.log(properties);
