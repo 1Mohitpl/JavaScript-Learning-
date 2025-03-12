509cecf3a85864ac4bd879d32e4132490a54ef23
Mohit kumar paul <mohit1paul@gmail.com>
2025-03-12 22:43:22 +0530

    access the private variable

--- OOps/first.js
+++ OOps/first.js
@@ -1,4 +1,4 @@
-class Product{
+class Person{
     // no-need of use let,var,const
     
     #name;
@@ -14,6 +14,27 @@ class Product{
           console.log("constructor is called");
           
     }
+
+    set setName (name) {
+           // setname is used for access the private variable into the class of an obect
+        if(typeof(name) != 'string'){
+            console.log("invalid name passed");
+            return;
+            
+        }
+        this.#name = name;
+    }
+
+    // setPrice (price){
+    //     if(price < 0) return;
+    //     this.
+    // }
+
+    get getName () {
+        // get the 
+        return this.#name;
+    }
+
     display() {
         // no-need to write function key word"
      console.log(this.#name, this.age, this.discription);
@@ -22,8 +43,11 @@ class Product{
 
 }
 
-const p = new Product("mohit", 24, "Software enginner"); // default constructor
+const p = new Person("mohit", 24, "Software enginner"); // default constructor
+
+p.setName = "tina"
+
+console.log(p.getName);   // with 'get', now don't have to call the function
 
 
-p.display();
 
