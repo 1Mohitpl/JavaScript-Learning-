f87c42e931c25bbb554a276a6e5cc0a013ce7f1f
Mohit kumar paul <mohit1paul@gmail.com>
2025-03-26 01:18:47 +0530

    own promise

--- Promise/02_learn.js
+++ Promise/02_learn.js
@@ -1,105 +1,117 @@
-const cart  = ["shirts", "shoes","toy"]
-
-createOrder(cart, function (orderId){
-    procedTopayment(orderId);
-});   // return order detils
+const cart = ["shirts", "shoes", "watch"];
 
 createOrder(cart)
-
-  // promise channing                 // console.log(promise);
-.then(function (orderId){
-    console.log(orderId);
-    return orderId;
-})
-.catch (function(error) {
+  .then(function (orderId) {
+    console.log("Order ID:", orderId);
+    return ordersummery(orderId);
+  })
+  .catch(function (error) {
     console.log(error.message);
-})
-.then(function(orderId) {
-    return procedTopayment(orderId)
-})
- .then(function (paymentInfo) {
-    console.log(paymentInfo)
- })
+  })
 
-.catch (function(error) {
+  .then(function (orderstatus) {
+    console.log(orderstatus);
+    
+    return procedTopayment(orderstatus.orderId);
+  })
+  .then (function (paymentInfo) {
+      return isorderplaced(paymentInfo);
+  })
+
+  .then (function (status) {
+    console.log(status);
+    
+  })
+  .catch(function (error) {
     console.log(error.message);
-})   
-
-proudcer
+  });
 
 function createOrder(cart) {
-
-    const prom = new Promise(function (resolve, reject) {
-          //createorderid
-          //validcart
-          //orderid
-        if(!validcart(cart)){
-          
-            const error = new Error("cart is not valid");
-            reject(error);
-        }
-
-        //logic for orderid
-
-        const orderid  = "76833";
-        if(orderid){
-            setTimeout(function (){
-                resolve(orderid);
-            }, 4000);
-        }
-    });
-
-    return prom;
+  return new Promise(function (resolve, reject) {
+    if (!validcart(cart)) {
+      const error = new Error("Cart is not valid");
+      reject(error);
+    } else {
+      const orderid = "76833";
+      setTimeout(function () {
+        resolve(orderid);
+      }, 4000);
+    }
+  });
 }
 
-function procedTopayment(orderId){
-    return new Promise (function (resolve, reject) {
-        resolve("payment is done")
-    });
+function ordersummery(orderId) {
+    return new Promise ( (resolve, reject) => {
+        resolve (
+            {
+                orderId : orderId,
+                products : cart
+            }
+        )
+    })
+
 }
-function validcart(cart){
-    return false;
+
+function procedTopayment(orderId) {
+  return new Promise(function (resolve, reject) {
+    resolve("Payment is done for order ID: " + orderId);
+  });
 }
+
+
+
+function isorderplaced(paymentInfo) {
+    return new Promise ( (resolve, reject) => {
+        setTimeout( () => {
+            resolve("order is successfully placed", 2000)
+        })
+    })
+}
+
+function validcart(cart) {
+  return true; // Ensuring the cart is valid if it contains items
+}
+
 
  // +++++++++++++++++++++++++++++++++++++++++++++++++++++
 // Learning ALL promise API
 
 
 
-const P1 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        reject("p1 sucess")
-    }, 4000)
-})
+// const P1 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         reject("p1 sucess")
+//     }, 4000)
+// })
 
-const P2 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        reject("p2 sucess")
-    }, 5000)
-})
+// const P2 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         reject("p2 sucess")
+//     }, 5000)
+// })
 
-const P3 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        reject("p3 fail")
-    }, 3000)
-})
+// const P3 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         reject("p3 fail")
+//     }, 3000)
+// })
 
-Promise.any([P1, P2, P3])
-  .then((result) =>{
-    console.log(result);
+// Promise.any([P1, P2, P3])
+//   .then((result) =>{
+//     console.log(result);
 
-  })
+//   })
 
-  .catch((err) =>{
-    console.error(err)
-    console.log(err.errors);
-  })
+//   .catch((err) =>{
+//     console.error(err)
+//     console.log(err.errors);
+//   })
 
 
 
-  const P4 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("p1 sucess")
-    }, 4000)
-})
+//   const P4 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve("p1 sucess")
+//     }, 4000)
+// })
 
