c25b795c43be0a9195a7e631b5e5cc626c4abc4d
Mohit kumar paul <mohit1paul@gmail.com>
2025-04-27 17:00:19 +0530

    call, apply and bind methode

--- Interview questions/main.js
+++ Interview questions/main.js
@@ -1,7 +1,36 @@
-async function getdata() {
-    return "namaste"
+let name1 = {
+    firstname : "Jhon",
+    lasktname : "dhe",
     
 }
+const printName  =  function(city, state){
+    console.log(this.firstname + " " +  this.lasktname + " " + city + " " +  state);
+}
 
-const ans  = getdata();
-ans.then(res => console.log(res));
+
+
+
+// let's take another object
+
+let name2 = {
+    firstname : "mohit",
+    lasktname : "paul",
+    // printName : function () {
+    //     console.log(this.firstname + " " + this.lasktname);   this is not a good way of writing this, call came into the capture 
+    // }
+}
+
+let name3 = {
+    firstname : "Shreya",
+    lasktname : "roy",
+}
+
+// so we can apply function borrowing methode, where we can borrowing function from, other objects
+printName.call(name1, "kolkata", "westbengal");
+
+printName.apply(name2, ["Raiganj", "westbengal"]);
+
+// bind methode
+
+const ans = printName.bind(name3, "Noida", "delhi");
+ans();
